c35ed1459c88b0ad5a3da7d65efc3ac72b833b55
manantpu <112652839+manantpu@users.noreply.github.com>
2022-10-28 01:39:55 -0700

    Update OpenFile.java

--- OpenFile.java
+++ OpenFile.java
@@ -1,6 +1,9 @@
+/*@author : Manasi Anantpurkar
+ */
 import java.io.File;
 import java.util.Scanner;
 import java.io.*;
+import java.util.ArrayList;
 
 public class OpenFile {
 
@@ -29,6 +32,7 @@ public class OpenFile {
 
 
     public static void fileip() throws IOException{
+        ArrayList<String> answer_arr = new ArrayList<String>(100);
         File file = new File("src/main/java/urinals.dat");
 
         Scanner scanner = new Scanner(file);
@@ -36,7 +40,9 @@ public class OpenFile {
             String x = scanner.next();
             System.out.println(x);
             int temp = counturinals.count_urinal(x);
+            answer_arr.add(String.valueOf(temp));
         }
+        CreateFile.addanstofile(answer_arr);
         scanner.close();
     }
 }
