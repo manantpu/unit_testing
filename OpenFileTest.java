e641d8e82ef56fc3e00626182b8dd00f5ba0b332
manantpu <112652839+manantpu@users.noreply.github.com>
2022-10-27 22:44:23 -0700

    IsFile Empty test case added

--- OpenFileTest.java
+++ OpenFileTest.java
@@ -1,12 +1,16 @@
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
-
+import java.io.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpenFileTest {
     @Test
     void isFileExists() {
         assertEquals(1, OpenFile.isFileExists(new File("src/main/java/abcd.dat")));
+    }
+
+    @Test
+    void isFileEmpty() {
+        assertEquals(1, OpenFile.isFileExists(new File("src/main/java/urinals.dat")));
     }
 }
