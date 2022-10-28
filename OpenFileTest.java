341e6f00c7bd5086514e54084854e0f76cf7f579
manantpu <112652839+manantpu@users.noreply.github.com>
2022-10-28 02:33:59 -0700

    Update OpenFileTest.java

--- OpenFileTest.java
+++ OpenFileTest.java
@@ -1,16 +1,19 @@
+/*@author : Manasi Anantpurkar
+ */
 import org.junit.jupiter.api.Test;
 import java.io.File;
-import java.io.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OpenFileTest {
     @Test
     void isFileExists() {
+        System.out.println("Check if the file exists");
         assertEquals(1, OpenFile.isFileExists(new File("src/main/java/abcd.dat")));
     }
 
     @Test
     void isFileEmpty() {
+        System.out.println("Check if the file is empty");
         assertEquals(1, OpenFile.isFileExists(new File("src/main/java/urinals.dat")));
     }
 }
